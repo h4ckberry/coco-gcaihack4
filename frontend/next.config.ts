eb5db0bbbc49afd207ca6ddf630c7e3cc3b47c45
h4ckberry <eno6k21@gmail.com>
2026-02-08 23:45:24 +0900

    chore: Firebase Hostingのメモリ設定と環境変数を追加

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    REASONING_ENGINE_ID: process.env.REASONING_ENGINE_ID,
+    SERVICE_ACCOUNT_EMAIL: process.env.SERVICE_ACCOUNT_EMAIL,
+  },
 };
 
 export default nextConfig;
